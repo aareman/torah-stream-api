955ea6928f65c66aaa76a6f517b5a629792ee8b7
Avi Areman <avi.areman@gmail.com>
2022-11-28 22:09:23 -0500

    feat: Add initial migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Series(models.Model):
+    key = models.CharField(null=True, max_length=255)
+    title = models.CharField(max_length=255)
+    podcast = models.URLField(max_length=255)
+    vodcast = models.URLField(null=True, max_length=255)
+
+
+class Shiur(models.Model):
+    series = models.ForeignKey(Series, on_delete=models.PROTECT, related_name="shiurim")
+    size = models.PositiveIntegerField()
+    title = models.CharField(max_length=255)
+    audio_src = models.URLField()
+    video_src = models.URLField(null=True)
+    track = models.PositiveIntegerField()
