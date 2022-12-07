8f6d93fdf19b92bc66ee0aff387f3bf781f6d707
Avi Areman <avi.areman@gmail.com>
2022-12-07 07:42:29 -0500

    change datetime to dates

--- api/models.py
+++ api/models.py
@@ -50,8 +50,8 @@ class Shiur(models.Model):
 class Category(TreeNode):
     name = models.CharField(max_length=255)
     position = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.name
